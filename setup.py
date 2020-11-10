4c593e073d0750d4db867ebc3bb3a3eb4bba54c6
Robert Ganowski @ Metoda <rganowski@metoda.com.pl>
2020-11-10 11:54:27 +0100

    Changed name otf the wheel.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="eightstars",
+    name="eight-stars",
     version="2.0.0",
     author="Robert Ganowski",
     author_email="robert.ganowski@gmail.com",
