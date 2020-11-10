f0f8c532048a1c2bfffda5aaeacdd07b12e22b22
Robert Ganowski @ Metoda <rganowski@metoda.com.pl>
2020-11-10 15:36:08 +0100

    Changed mind. It's gonna to be 1.0.0 version.  That' realy first public release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="eight-stars",
-    version="2.0.0",
+    version="1.0.0",
     author="Robert Ganowski",
     author_email="robert.ganowski@gmail.com",
     description="Simple stars geometries",
